55c07bf41ad9b28919e26ad8236c4bafce023e1e
Lalit Mehra <lokeshmehra7039@gmail.com>
2026-03-03 14:46:02 +0530

    add route

--- backend/index.js
+++ backend/index.js
@@ -200,6 +200,10 @@ app.use(express.json());
 
 app.use('/auth',authRoute);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.get('/addHoldings',async(req,res)=>{
   let allHoldings = await HoldingsModel.find({});
   res.json(allHoldings);
